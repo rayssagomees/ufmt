70ad4cfc8c610603854275ce1ceb102bf4bbb5c5
raysantori <contato@raysantori.com>
2023-03-17 21:04:48 -0300

    feat create, read and append vs._2

---
+++ programacao/pascal/class/appendTextFile_2.pas
@@ -0,0 +1,26 @@
+program appendTextFile_2;
+
+uses 
+    SysUtils ;
+  
+const
+    ARQ_NOME = 'eq2G.txt';
+
+var
+    arquivo: Text; // FileText
+
+begin
+    // associa a variável "arquivo" ao arquivo "dados.txt"
+    Assign(arquivo, ARQ_NOME);
+
+    Append(arquivo); // adiciona mais dados
+
+    writeln(arquivo, '4 -1 3');
+    writeln(arquivo, '5 1 -3');
+
+    Close(arquivo); // fecha o arquivo "dados.txt"
+
+    writeln('Arquivo ', ARQ_NOME, ' Criado');
+    writeln('Fim.');    
+    readln;
+end.
---
+++ programacao/pascal/class/createTextFile_2.pas
@@ -0,0 +1,27 @@
+program createTextFile_2;
+
+uses 
+  SysUtils ;
+  
+const
+  ARQ_NOME = 'eq2G.txt';
+
+var
+  arquivo: Text; // FileText
+
+begin
+  // associa a variável "arquivo" ao arquivo "dados.txt"
+  Assign(arquivo, ARQ_NOME);
+  
+  Rewrite(arquivo); // cria o arquivo "dados.txt"
+  
+  writeln(arquivo, 'A B C');
+  writeln(arquivo, '-1 -2 3');
+  writeln(arquivo, '1 2 -3');
+  
+  Close(arquivo); // fecha o arquivo "dados.txt"
+  
+  writeln('Arquivo ', ARQ_NOME, ' Criado');
+  writeln('Fim.');    
+  readln;
+end.
