76576289dd1b277ba13c449d480276fc4e3f5334
Rayssa Gomes <contato@raysantori.com>
2023-04-14 10:52:01 -0300

    Update eq2gsolver.c

--- eq2gsolver.c
+++ eq2gsolver.c
@@ -10,16 +10,32 @@ int main(void)
   int a, b, c; /* declaração de variável inteira */
   float delta;  /* declaração de variável real */
 
-  a = 1;
+  /* %d = mostra como decimal */
+  /*a = 1;
   b = -3;
-  c = 2;
+  c = 2; */
+
+  printf("Entre com a: "); scanf("%f", &a);
+  printf("Entre com b: "); scanf("%f", &b);
+  printf("Entre com c: "); scanf("%f", &c);
+
+  printf("\na = %3.2f \n", a);
+  printf("b = %3.2f \n", b);
+  printf("c = %3.2f \n\n", c);
 
   delta = b*b-4*a*c;
 
-  printf("a = %d \n", a); /* %d = mostra como decimal */
-  printf("b = %d \n", b); /* %d = mostra como decimal */
-  printf("c = %d \n", c); /* %d = mostra como decimal */
-  printf("Delta = %f", delta); /* %f = mostra como float */
+  if (delta >= 0){
+    float x1 = (-b+sqrt(delta))/(2*a);
+    float x2 = (-b-sqrt(delta))/(2*a);
+
+    printf("Delta = %3.2f \n\n", delta); /* %f = mostra como float; 0.3 quantidade de decimais na frente do tipo */
+    printf("x1 = %3.2f \n", x1);
+    printf("x2 = %3.2f \n\n", x2);
+  }
+  else { /* delta negativo */
+    printf("Delta = %0.2f \n\n", delta); /* %f = mostra como float; 0.3 quantidade de decimais na frente do tipo */
+  }
 
   return 0;
 }
