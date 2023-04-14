a226d93944e02784d1aa4adb0553d9e8fa35d793
Rayssa Gomes <contato@raysantori.com>
2023-04-14 10:08:30 -0300

    Update eq2gsolver.c

--- eq2gsolver.c
+++ eq2gsolver.c
@@ -7,6 +7,19 @@
 
 int main(void)
 {
-  printf("\n Hello C World! \n");
+  int a, b, c; /* declaração de variável inteira */
+  float delta;  /* declaração de variável real */
+
+  a = 1;
+  b = -3;
+  c = 2;
+
+  delta = b*b-4*a*c;
+
+  printf("a = %d \n", a); /* %d = mostra como decimal */
+  printf("b = %d \n", b); /* %d = mostra como decimal */
+  printf("c = %d \n", c); /* %d = mostra como decimal */
+  printf("Delta = %f", delta); /* %f = mostra como float */
+
   return 0;
 }
