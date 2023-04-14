53d34c7394cf1148ccc030be85866b422760d975
Rayssa Gomes <contato@raysantori.com>
2023-04-14 09:37:58 -0300

    Update class_04-14.c

--- class_04-14.c
+++ class_04-14.c
@@ -1,13 +1,16 @@
 /* class_04-14.c */
 /* C constrói um sistema operacional */
 
+/* https://github.com/jmpessoa/LazCWizard */
+/* http://download.savannah.gnu.org/releases/tinycc/ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-int main () /* main = programa principal, que é uma função, void dá função sem parâmetro, ou seja (), pode ser com ou sem */
-(
+int main(void) /* main = programa principal, que é uma função, void dá função sem parâmetro, ou seja (), pode ser com ou sem */
+{
   printf("\n Hello C World! \n"); /* \n = nova linha */
   return 0;
-)
+}  
