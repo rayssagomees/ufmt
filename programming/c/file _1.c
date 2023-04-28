f1c604997edf5955ec95bc093693a82a5b94972d
Rayssa Gomes <contato@raysantori.com>
2023-04-28 10:32:03 -0300

    Update file _1.c

--- programming/c/file _1.c
+++ programming/c/file _1.c
@@ -4,24 +4,29 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
+                                                   
 int main(void)
 {
-FILE *archive1 = fopen("data.txt","w");
+  int a;
+  char c;
+
+  FILE *archive1 = fopen("data.txt","w"); // declara e cria o arquivo
+  // w = write, * variável de arquivo
 
   if(archive1 == NULL){
-     printf("Não consigo abrir o arquivo"); exit(-1);
+     printf("Não consigo abrir o arquivo.");
+     exit(-1); // sai, deu certo = 0, deu errado = -1
   }
 
-  char str1[] = "Olá mundo!";
+  char str1[] = "Olá mundo!"; // str1 é uma variável vetor, ou seja, frase
   int count = strlen(str1);
 
   for(int i=0; i < count; i++) {
-     putc(str1[i], archive1);
+     putc(str1[i], archive1); // coloca coisas dentro do arquivo
   }
 
   fclose(archive1);
   printf("\n Arquivo criado. \n");
 
-return 0;
+  return 0;
 }
